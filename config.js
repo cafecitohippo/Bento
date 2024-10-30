5bf8dbcd579d5fa50654de5009dea2855130c833
cafecitohippo <128174184+cafecitohippo@users.noreply.github.com>
2024-10-30 16:12:05 -0400

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const CONFIG = {
 
 	// Weather
 	weatherKey: '69fde5a4383272f5c5b218021a150dcc', // Write here your API Key
-	weatherIcons: 'White', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherIcons: 'Nord', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'F', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
